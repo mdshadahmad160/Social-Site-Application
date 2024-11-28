770dfc9c3d124f9487655b53f4342efcc4bf935b
Md Shad Ahmad <136076679+mdshadahmad160@users.noreply.github.com>
2024-11-28 18:06:00 +0530

    initial commit

---
+++ src/main/java/com/social/api/SocialSiteApplication.java
@@ -0,0 +1,13 @@
+package com.social.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialSiteApplication.class, args);
+	}
+
+}
